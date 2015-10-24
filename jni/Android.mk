660a9d20948f855130725c1441640602c049a08a
Pierre-Hugues Husson <husson@archos.com>
2015-10-25 01:55:29 +0200

    Build sepolicy-inject

--- jni/Android.mk
+++ jni/Android.mk
@@ -17,3 +17,16 @@ LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_STATIC_LIBRARIES := libc libcutils
 LOCAL_SRC_FILES := repack.c
 include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := sepolicy-inject
+LOCAL_MODULE_TAGS := optional
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_STATIC_LIBRARIES := libc libcutils libsepol libselinux
+LOCAL_SRC_FILES := sepolicy-inject/sepolicy-inject.c
+LOCAL_C_INCLUDES := jni/libselinux/include/ jni/libsepol/include/
+LOCAL_CFLAGS += -std=gnu11
+include $(BUILD_EXECUTABLE)
+
+include $(my_path)/libselinux/Android.mk
+include $(my_path)/libsepol/Android.mk
