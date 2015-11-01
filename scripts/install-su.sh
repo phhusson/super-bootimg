c8c86c157bc736a0ba27dce18d9ed4db3d744e44
Pierre-Hugues Husson <husson@archos.com>
2015-11-01 22:16:38 +0100

    (re)add a su_daemon context

--- scripts/install-su.sh
+++ scripts/install-su.sh
@@ -50,10 +50,18 @@ function allow() {
 function allowSuClient() {
 	#All domain-s already have read access to rootfs
 	allow $1 rootfs file "execute_no_trans execute" #TODO: Why do I need execute?!? (on MTK 5.1, kernel 3.10)
-	allow $1 su unix_stream_socket "connectto getopt"
+	allow $1 su_daemon unix_stream_socket "connectto getopt"
 
 	allow $1 su_device dir "search read"
 	allow $1 su_device sock_file "read write"
+	allow su_daemon $1 "fd" "use"
+
+	allow su_daemon $1 fifo_file "read write getattr ioctl"
+
+	#Read /proc/callerpid/cmdline in from_init, drop?
+	allow su_daemon "$1" "dir" "search"
+	allow su_daemon "$1" "file" "read open"
+	allow su_daemon "$1" "lnk_file" "read"
 }
 
 #allowLog <scontext>
@@ -64,15 +72,78 @@ function allowLog() {
 	allow logd $1 file "read open getattr"
 }
 
+function suDaemonTo() {
+	allow su_daemon $1 "process" "transition"
+	allow su_daemon $1 "process" "siginh rlimitinh noatsecure"
+}
+
+function suRights() {
+	#am start superuser apk
+	allow servicemanager $1 "dir" "search read"
+	allow servicemanager $1 "file" "open read"
+	allow servicemanager $1 "process" "getattr"
+	allow servicemanager $1 "binder" "transfer"
+}
+
+function suDaemonRights() {
+	allow su_daemon rootfs file "entrypoint"
+
+	allow su_daemon su_daemon "dir" "search read"
+	allow su_daemon su_daemon "file" "read write open"
+	allow su_daemon su_daemon "lnk_file" "read"
+	allow su_daemon su_daemon "unix_dgram_socket" "create connect write"
+	allow su_daemon su_daemon "unix_stream_socket" "create bind listen accept getopt read write"
+
+	allow su_daemon devpts chr_file "read write open"
+	allow su_daemon untrusted_app_devpts chr_file "read write open"
+
+	allow su_daemon su_daemon "capability" "setuid setgid"
+
+	#Access to /data/data/me.phh.superuser/xxx
+	allow su_daemon app_data_file "dir" "getattr search write add_name"
+	allow su_daemon app_data_file "file" "getattr read open lock"
+
+	#FIXME: This shouldn't exist
+	allow su_daemon su_daemon "capability" "dac_override sys_ptrace"
+
+	#I see some forks() before even getting a su call, normal?
+	allow su_daemon su_daemon "process" "fork sigchld"
+
+	#toolbox needed for log
+	allow su_daemon toolbox_exec "file" "execute read open execute_no_trans"
+
+	#Create /dev/me.phh.superuser. Could be done by init
+	allow su_daemon device "dir" "write add_name"
+	allow su_daemon su_device "dir" "create setattr remove_name add_name"
+	allow su_daemon su_device "sock_file" "create unlink"
+
+	#Allow su daemon to start su apk
+	allow su_daemon zygote_exec "file" "execute read open execute_no_trans"
+
+	#Send request to APK
+	allow su_daemon su_device dir "search write add_name"
+
+	#Allow su_daemon to switch to su or su_sensitive
+	allow su_daemon su_daemon "process" "setexec"
+
+	#Allow su_daemon to execute a shell (every commands are supposed to go through a shell)
+	allow su_daemon shell_exec file "execute read open"
+
+	allow su_daemon su_daemon "capability" "chown"
+
+	suDaemonTo su
+}
+
 cp "$scriptdir"/bin/su sbin/su
 if [ -f "sepolicy" ];then
 	#Create domains if they don't exist
 	"$scriptdir"/bin/sepolicy-inject -z su -P sepolicy
 	"$scriptdir"/bin/sepolicy-inject -z su_device -P sepolicy
+	"$scriptdir"/bin/sepolicy-inject -z su_daemon -P sepolicy
 
 	#Autotransition su's socket to su_device
-	"$scriptdir"/bin/sepolicy-inject -s su -f device -c file -t su_device -P sepolicy
-	"$scriptdir"/bin/sepolicy-inject -s su -f device -c dir -t su_device -P sepolicy
+	"$scriptdir"/bin/sepolicy-inject -s su_daemon -f device -c file -t su_device -P sepolicy
+	"$scriptdir"/bin/sepolicy-inject -s su_daemon -f device -c dir -t su_device -P sepolicy
 	allow su_device tmpfs filesystem "associate"
 
 	#Transition from untrusted_app to su_client
@@ -81,13 +152,15 @@ if [ -f "sepolicy" ];then
 	allowSuClient untrusted_app
 
 	#Allow init to execute su daemon/transition
-	allow init su process "transition"
+	allow init su_daemon process "transition"
+	suDaemonRights
 
 	allowLog su
+	suRights su
 
 	#Need to set su_device/su as trusted to be accessible from other categories
 	"$scriptdir"/bin/sepolicy-inject -a mlstrustedobject -s su_device -P sepolicy
-	"$scriptdir"/bin/sepolicy-inject -a mlstrustedsubject -s su  -P sepolicy
+	"$scriptdir"/bin/sepolicy-inject -a mlstrustedsubject -s su_daemon -P sepolicy
 	if [ "$2" == "eng" ];then
 		"$scriptdir"/bin/sepolicy-inject -Z su -P sepolicy
 	else
@@ -97,7 +170,7 @@ if [ -f "sepolicy" ];then
 fi
 
 sed -i -E '/on init/a \\tchmod 0755 /sbin' init.rc
-echo -e 'service su /sbin/su --daemon\n\tclass main\n\tseclabel u:r:su:s0\n' >> init.rc
+echo -e 'service su /sbin/su --daemon\n\tclass main\n\tseclabel u:r:su_daemon:s0\n' >> init.rc
 
 echo -e 'sbin/su\ninit.rc\nsepolicy\nfile_contexts' | cpio -o -H newc > ramdisk2
 
