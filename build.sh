230e014bdcd052bdee4d6eb3a5cf64d342987c3f
Pierre-Hugues Husson <husson@archos.com>
2015-11-07 17:00:53 +0100

    build.sh now builds power level

--- build.sh
+++ build.sh
@@ -25,6 +25,15 @@ find output -name orig-boot.img |while read i;do
 			mv -f new-boot.img boot-su-user.img
 		fi
 
+		#user su
+		bash "$scriptdir/bootimg.sh" orig-boot.img "$scriptdir/su/changes.sh" power
+		if [ -f new-boot.img.signed ];then
+			mv -f new-boot.img.signed boot-su-power.img
+			rm -f new-boot.img
+		else
+			mv -f new-boot.img boot-su-power.img
+		fi
+
 
 	popd
 done
