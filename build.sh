20608a692f78242caa79c6a46ed06a25021a0fc0
Pierre-Hugues Husson <husson@archos.com>
2015-12-14 10:07:23 +0100

    Remove "power" build, add "noverity" build

--- build.sh
+++ build.sh
@@ -25,15 +25,17 @@ find output -name orig-boot.img |while read i;do
 			mv -f new-boot.img boot-su-user.img
 		fi
 
-		#user su
-		bash "$scriptdir/bootimg.sh" orig-boot.img "$scriptdir/su/changes.sh" power
+		#Remove old power su
+		rm -f boot-su-power.img
+
+		#noverity eng su
+		bash "$scriptdir/bootimg.sh" orig-boot.img "$scriptdir/su/changes.sh" eng noverity
 		if [ -f new-boot.img.signed ];then
-			mv -f new-boot.img.signed boot-su-power.img
+			mv -f new-boot.img.signed boot-su-noverity.img
 			rm -f new-boot.img
 		else
-			mv -f new-boot.img boot-su-power.img
+			mv -f new-boot.img boot-su-noverity.img
 		fi
-
 
 	popd
 done
