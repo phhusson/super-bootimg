e36bd5837fefe6021a77ff4e8e6146e0457a350b
Pierre-Hugues Husson <husson@archos.com>
2015-12-28 13:51:28 +0100

    Refactor build.sh, img names include device,  firmware version, number of commits. Add nocrypt variant

--- build.sh
+++ build.sh
@@ -1,41 +1,39 @@
 #!/bin/bash
 
 set -e
-
 scriptdir="$PWD/scripts/"
+rev="$(cd $scriptdir; git rev-list HEAD --count)"
+function putBoot() {
+	version="$(basename "$PWD")"
+	t="$(dirname "$PWD")"
+	device="$(basename "$t")"
+	variant="$1"
+	if [ -f new-boot.img.signed ];then
+		mv -f new-boot.img.signed boot-${device}-${version}-su-${variant}-r${rev}.img
+		rm -f new-boot.img
+	else
+		mv -f new-boot.img boot-${device}-${version}-su-${variant}-r${rev}.img
+	fi
+}
+
 find output -name orig-boot.img |while read i;do
 	pushd "$(dirname "$i")"
 
-
+		rm -f boot-*.img
 		#eng su
 		bash "$scriptdir/bootimg.sh" orig-boot.img "$scriptdir/su/changes.sh" eng
-		if [ -f new-boot.img.signed ];then
-			mv -f new-boot.img.signed boot-su-eng.img
-			rm -f new-boot.img
-		else
-			mv -f new-boot.img boot-su-eng.img
-		fi
+		putBoot eng
 
 		#user su
 		bash "$scriptdir/bootimg.sh" orig-boot.img "$scriptdir/su/changes.sh"
-		if [ -f new-boot.img.signed ];then
-			mv -f new-boot.img.signed boot-su-user.img
-			rm -f new-boot.img
-		else
-			mv -f new-boot.img boot-su-user.img
-		fi
-
-		#Remove old power su
-		rm -f boot-su-power.img
+		putBoot user
 
 		#noverity eng su
 		bash "$scriptdir/bootimg.sh" orig-boot.img "$scriptdir/su/changes.sh" eng noverity
-		if [ -f new-boot.img.signed ];then
-			mv -f new-boot.img.signed boot-su-noverity.img
-			rm -f new-boot.img
-		else
-			mv -f new-boot.img boot-su-noverity.img
-		fi
+		putBoot noverity
 
+		#noverity nocrypt eng su
+		bash "$scriptdir/bootimg.sh" orig-boot.img "$scriptdir/su/changes.sh" eng noverity nocrypt
+		putBoot nocrypt
 	popd
 done
