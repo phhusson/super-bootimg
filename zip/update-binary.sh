93ac5f638a84ea71daa53fb761604784af02e7fc
Pierre-Hugues Husson <husson@archos.com>
2016-01-31 23:19:29 +0100

    Update ui_prints

--- zip/update-binary.sh
+++ zip/update-binary.sh
@@ -22,6 +22,7 @@ ui_print() {
 	echo "ui_print $1" >> /proc/self/fd/$fd
 }
 
+ui_print "Installing seSuperuser..."
 
 rm -Rf /tmp/superuser
 mkdir -p /tmp/superuser
@@ -32,12 +33,12 @@ fstab="/etc/recovery.fstab"
 [ ! -f "$fstab" ] && fstab="/etc/recovery.fstab.bak"
 bootimg="$(grep -E '\b/boot\b' "$fstab" |grep -oE '/dev/[a-zA-Z0-9_./-]*')"
 if [ -z "$bootimg" ];then
-	ui_print "Couldn't find boot.img partition"
+	ui_print "seSuperuser: Couldn't find boot.img partition"
 	exit 1
 fi
 
-ui_print "Found bootimg @ $bootimg"
+ui_print "seSuperuser: Found bootimg @ $bootimg"
 sh -x ../bootimg.sh $bootimg $modes
-ui_print "Generated $pwd/new-boot.img"
+ui_print "seSuperuser: Generated $pwd/new-boot.img"
 dd if=new-boot.img of=$bootimg bs=8192
-ui_print "Flashed root-ed boot.img"
+ui_print "seSuperuser: Successfully flashed root-ed boot.img"
