37ed500c2067603083667dcaf9d445877b2b17eb
Pierre-Hugues Husson <husson@archos.com>
2016-03-16 22:22:31 +0100

    Update Android.mk to modularized

--- jni/Android.mk
+++ jni/Android.mk
@@ -7,7 +7,7 @@ LOCAL_MODULE := bootimg-extract
 LOCAL_MODULE_TAGS := optional
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_LDFLAGS := -static
-LOCAL_STATIC_LIBRARIES := libc libcutils
+LOCAL_STATIC_LIBRARIES := libc libcutils libbootimg
 LOCAL_SRC_FILES := extract.c
 include $(BUILD_EXECUTABLE)
 
@@ -20,15 +20,15 @@ LOCAL_STATIC_LIBRARIES := libc libcutils
 LOCAL_SRC_FILES := repack.c
 include $(BUILD_EXECUTABLE)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := strip-cpio
-LOCAL_MODULE_TAGS := optional
-LOCAL_FORCE_STATIC_EXECUTABLE := true
-LOCAL_LDFLAGS := -static
-LOCAL_STATIC_LIBRARIES := libc libcutils
-LOCAL_SRC_FILES := strip-cpio.c
-LOCAL_CFLAGS += -std=gnu11
-include $(BUILD_EXECUTABLE)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := strip-cpio
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_FORCE_STATIC_EXECUTABLE := true
+#LOCAL_LDFLAGS := -static
+#LOCAL_STATIC_LIBRARIES := libc libcutils
+#LOCAL_SRC_FILES := strip-cpio.c
+#LOCAL_CFLAGS += -std=gnu11
+#include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := sepolicy-inject
@@ -40,6 +40,12 @@ LOCAL_SRC_FILES := sepolicy-inject/sepolicy-inject.c
 LOCAL_C_INCLUDES := jni/libselinux/include/ jni/libsepol/include/
 LOCAL_CFLAGS += -std=gnu11
 include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libbootimg
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := lib-parse.c
+include $(BUILD_STATIC_LIBRARY)
 
 include $(my_path)/libselinux/Android.mk
 include $(my_path)/libsepol/Android.mk
