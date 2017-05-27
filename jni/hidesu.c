c9709a7c8a5a48caacf88f4b971e8c49c50119c3
Davis Mosenkovs <davikovs@gmail.com>
2017-05-28 00:01:18 +0300

    hidesu: Improve race conditions + other things
    Various fixes/improvements:
    * Fix handle leak (fd in disableSu()).
    * Rise priority to give advantage in race conditions.
    * Use SIGSTOP and SIGCONT on app processes to give advantage in race
    conditions. Idea taken from Magiskhide (by John Wu (@topjohnwu)).
    * Minor performance optimizations.

--- jni/hidesu.c
+++ jni/hidesu.c
@@ -24,30 +24,30 @@ typedef unsigned short int sa_family_t;
 //WARNING: Calling this will change our current namespace
 //We don't care because we don't want to run from here anyway
 int disableSu(int pid) {
-	char path[64];
 	char buffer[512];
 	char *p, *p2;
 	int ret = 0;
-	snprintf(path, 64, "/proc/%d/ns/mnt", pid);
-	int fd = open(path, O_RDONLY);
+	snprintf(buffer, sizeof(buffer), "/proc/%d/ns/mnt", pid);
+	int fd = open(buffer, O_RDONLY);
 	if(fd == -1) return 2;
 	int res = syscall(SYS_setns, fd, 0);
+	close(fd);
 	if(res == -1) return 3;
 
 	// unmount the tmpfs from /sbin
-	res = umount("/sbin", MNT_DETACH);
+	res = umount2("/sbin", MNT_DETACH);
 	if(res == -1) ret |= 4;
 
 	// unmount anything under /system/
-	snprintf(path, 64, "/proc/%d/mounts", pid);
-	FILE *mf = fopen(path, "r");
+	snprintf(buffer, sizeof(buffer), "/proc/%d/mounts", pid);
+	FILE *mf = fopen(buffer, "r");
 	if(mf == NULL) return ret | 8;
 	while(fgets(buffer, sizeof(buffer), mf) != NULL) {
 		if((p = strchr(buffer, ' ')) == NULL) continue;
 		p++;
 		if((p2 = strchr(p, ' ')) == NULL) continue;
 		*p2 = 0;
-		if(strncmp(p, "/system/", 8)==0 && p2-p>8) {
+		if(p2-p>8 && strncmp(p, "/system/", 8)==0) {
 			res = umount2(p, MNT_DETACH);
 			if(res == -1) ret |= 16;
 		}
@@ -58,6 +58,9 @@ int disableSu(int pid) {
 }
 
 int main(int argc, char **argv, char **envp) {
+	// rise priority to get advantage in race conditions
+	nice(-15);
+
 	FILE *p = popen("while true;do logcat -b events -v raw -s am_proc_start;sleep 1;done", "r");
 	while(!feof(p)) {
 		//Format of am_proc_start is (as of Android 5.1 and 6.0)
@@ -99,8 +102,10 @@ int main(int argc, char **argv, char **envp) {
 				strcmp(processName, "com.airwatch.androidagent") == 0
 				) {
 
+			kill(pid, SIGSTOP);
 			printf("Disabling for PID = %d, UID = %d\n", pid, uid);
 			disableSu(pid);
+			kill(pid, SIGCONT);
 		}
 	}
 
