36e918632436e5fa5dba3af1bde6e1a47e778a0d
Davis Mosenkovs <davikovs@gmail.com>
2017-05-30 00:34:05 +0300

    hidesu: Prepare tmpfs inside hidesu
    This removes the need of shellscript - improves performance (advantage
    in race conditions) and removes shell dependency

--- jni/hidesu.c
+++ jni/hidesu.c
@@ -3,6 +3,8 @@ typedef unsigned short int sa_family_t;
 #define _LINUX_TIME_H
 #define _GNU_SOURCE
 #define MNT_DETACH 2
+#define XATTR_NAME_SELINUX "security.selinux"
+#define DEFAULT_CONTEXT "u:object_r:rootfs:s0"
 #include <sys/types.h>
 #include <string.h>
 #include <sys/stat.h>
@@ -20,6 +22,9 @@ typedef unsigned short int sa_family_t;
 #include <sys/glibc-syscalls.h>
 #include <asm/unistd.h>
 #include <strings.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/xattr.h>
 
 //WARNING: Calling this will change our current namespace
 //We don't care because we don't want to run from here anyway
@@ -57,9 +62,114 @@ int disableSu(int pid) {
 	return ret;
 }
 
+int makeSbinTmpfs() {
+	DIR *sd;
+	struct dirent *dir;
+	struct stat sb, sb2;
+	char sfile[256];
+	char dfile[256];
+	char buffer[256];
+	int sfd, dfd, res;
+	size_t bytes, copied;
+
+	// check that /sbin is NOT already mounted
+	stat("/", &sb);
+	stat("/sbin", &sb2);
+	if(sb.st_dev != sb2.st_dev) return 0;
+
+	strcpy(sfile, "/sbin/");
+	strcpy(dfile, "/dev/sbin.tmp/");
+	mkdir("/dev/sbin.tmp", 0700);
+	if(mount("tmpfs", "/dev/sbin.tmp", "tmpfs", 0, "size=64m,nr_inodes=8192,mode=755,uid=0,gid=0") == -1) return 1;
+
+	res = getxattr("/sbin", XATTR_NAME_SELINUX, buffer, sizeof(buffer) - 1);
+	if(res != -1) {
+		buffer[res] = 0;
+		res = setxattr("/dev/sbin.tmp", XATTR_NAME_SELINUX, buffer, strlen(buffer) + 1, 0);
+	}
+	if(res == -1) {
+		setxattr("/dev/sbin.tmp", XATTR_NAME_SELINUX, DEFAULT_CONTEXT, strlen(DEFAULT_CONTEXT) + 1, 0);
+	}
+
+	if((sd = opendir("/sbin")) == NULL) {
+		umount2("/dev/sbin.tmp", MNT_DETACH);
+		return 2;
+	}
+	while((dir = readdir(sd)) != NULL) {
+		// copy files (with symlink support), copy mode/owner + SELinux context
+		strncpy(sfile+6, dir->d_name, sizeof(sfile)-6);
+		strncpy(dfile+14, dir->d_name, sizeof(dfile)-14);
+		switch(dir->d_type) {
+		case DT_REG:
+			sfd = open(sfile, O_RDONLY);
+			if(sfd == -1 || fstat(sfd, &sb) == -1) {
+				umount2("/dev/sbin.tmp", MNT_DETACH);
+				return 3;
+			}
+			if((dfd = open(dfile, O_WRONLY | O_CREAT | O_EXCL, sb.st_mode)) == -1) {
+				umount2("/dev/sbin.tmp", MNT_DETACH);
+				return 3;
+			}
+			copied = 0;
+			while((bytes = sendfile(dfd, sfd, NULL, (size_t) sb.st_size)) > 0) copied += bytes;
+			if(copied != (size_t) sb.st_size) {
+				umount2("/dev/sbin.tmp", MNT_DETACH);
+				return 3;
+			}
+			fchown(dfd, sb.st_uid, sb.st_gid);
+			fchmod(dfd, sb.st_mode);
+			res = fgetxattr(sfd, XATTR_NAME_SELINUX, buffer, sizeof(buffer) - 1);
+			if(res != -1) {
+				buffer[res] = 0;
+				res = fsetxattr(dfd, XATTR_NAME_SELINUX, buffer, strlen(buffer) + 1, 0);
+			}
+			if(res == -1) {
+				fsetxattr(dfd, XATTR_NAME_SELINUX, DEFAULT_CONTEXT, strlen(DEFAULT_CONTEXT) + 1, 0);
+			}
+			close(sfd);
+			close(dfd);
+			break;
+		case DT_LNK:
+			if((res = readlink(sfile, buffer, sizeof(buffer)-1)) == -1) {
+				umount2("/dev/sbin.tmp", MNT_DETACH);
+				return 3;
+			}
+			buffer[res] = 0;
+			if(symlink(buffer, dfile) == -1) {
+				umount2("/dev/sbin.tmp", MNT_DETACH);
+				return 3;
+			}
+			if(lstat(sfile, &sb) == 0) lchown(dfile, sb.st_uid, sb.st_gid);
+			res = lgetxattr(sfile, XATTR_NAME_SELINUX, buffer, sizeof(buffer) - 1);
+			if(res != -1) {
+				buffer[res] = 0;
+				res = lsetxattr(dfile, XATTR_NAME_SELINUX, buffer, strlen(buffer) + 1, 0);
+			}
+			if(res == -1) {
+				lsetxattr(dfile, XATTR_NAME_SELINUX, DEFAULT_CONTEXT, strlen(DEFAULT_CONTEXT) + 1, 0);
+			}
+			break;
+		}
+	}
+	closedir(sd);
+	mount("tmpfs", "/dev/sbin.tmp", "tmpfs", MS_REMOUNT | MS_RDONLY, "size=64m,nr_inodes=8192,mode=755,uid=0,gid=0");
+	if(mount("/dev/sbin.tmp", "/sbin", NULL, MS_BIND, NULL) == -1) {
+		umount2("/dev/sbin.tmp", MNT_DETACH);
+		return 4;
+	}
+	umount2("/dev/sbin.tmp", MNT_DETACH);
+	rmdir("/dev/sbin.tmp");
+
+	return 0;
+}
+
 int main(int argc, char **argv, char **envp) {
 	// rise priority to get advantage in race conditions
 	nice(-15);
+	if(makeSbinTmpfs() != 0) {
+		usleep(200 * 1000);
+		return 1;
+	}
 
 	FILE *p = popen("while true;do logcat -b events -v raw -s am_proc_start;sleep 1;done", "r");
 	while(!feof(p)) {
